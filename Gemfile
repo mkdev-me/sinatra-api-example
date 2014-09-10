cec03582a6cc6b884592757237243bc3ed043729
Kirill Shirinkin <fodojyko@gmail.com>
2014-09-10 16:50:16 +0200

    simplest json api

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem "sinatra-activerecord"
+gem 'sinatra-activerecord'
 gem 'sqlite3'
 gem 'rake'
+gem 'sinatra-contrib'
--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/json'
 set :database, { adapter: 'sqlite3', database: 'transactions.sqlite' }
 
 class Transaction < ActiveRecord::Base
   validates :amount, presence: true
+end
+
+post "/transactions" do
+  transaction = Transaction.create(params[:transaction])
+  json status: :ok, transaction: transaction
+end
+
+get "/transactions" do
+  json transactions: Transaction.all
+end
+
+get "/transactions/:id" do
+  json transaction: Transaction.find(params[:id])
+end
+
+delete "/transactions/:id" do
+  transaction = Transaction.find(params[:id])
+  transaction.destroy!
+  json status: :ok
+end
+
+put "/transactions/:id" do
+  transaction = Transaction.find(params[:id])
+  transaction.update_attributes(params[:transaction])
+  json status: :ok, transaction: transaction
 end
